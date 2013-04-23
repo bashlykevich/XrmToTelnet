93701cc097b3240961e56d403f45be6293df1fd3
bashlykevich <bashlykevich@gmail.com>
2013-04-23 12:32:58 +0200

    Реструктуризация проекта

---
+++ XrmToTelnet/Netland/TaskSmsSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XrmToTelnet.DataAccess;
+
+namespace XrmToTelnet.Netland
+{
+    class TaskSmsSender
+    {
+        public void SendSms(string[] args)
+        {
+            List<tbl_Task> tasks = DataConnector.Instance.ProcessUrgentTasks();
+
+            foreach (tbl_Task t in tasks)
+            {
+                string sms = "Terrasoft: " + Transliter.Transliterate(t.Title);
+                if (sms.Length > 160)
+                {
+                    sms.Remove(157);
+                    sms += "...";
+                }
+
+                List<string> nums = DataConnector.Instance.GetContactPhoneNumbers(t.OwnerID);
+                if (nums.Count == 0)
+                {
+                    sms = "Terrasoft: not found " + Transliter.Transliterate(DataConnector.Instance.GetContactName(t.OwnerID));
+                    string AdminPhone = System.Configuration.ConfigurationManager.AppSettings["admin_phone"];
+                    GSMGate.SendSms(AdminPhone, sms);
+                }
+                foreach (string num in nums)
+                {
+                    GSMGate.SendSms(num, sms);
+                }
+            }
+        }
+    }
+}
--- XrmToTelnet/Program.cs
+++ XrmToTelnet/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
 using XrmToTelnet.DataAccess;
+using XrmToTelnet.Netland;
 
 namespace XrmToTelnet
 {
@@ -15,36 +16,8 @@ namespace XrmToTelnet
 
         static void Main(string[] args)
         {
-            List<tbl_Task> tasks = DataConnector.Instance.ProcessUrgentTasks();
-            //debugConsole.WriteLine("Urgent tasks: " + tasks.Count);
-            //debugList<string> nophones = new List<string>();
-            //debugint i = 1;
-            foreach (tbl_Task t in tasks)
-            {
-                string sms = "Terrasoft: " + Transliter.Transliterate(t.Title);
-                if(sms.Length>160)                    
-                {
-                    sms.Remove(157);
-                    sms+="...";
-                }                
-                //debugstring contact = DataConnector.Instance.GetContactName(t.OwnerID);
-                //debugConsole.WriteLine("");
-                //debugConsole.WriteLine("Task #" + i++ + " title: " + );
-                //debugConsole.WriteLine("Task owner: " + contact);
-
-                List<string> nums = DataConnector.Instance.GetContactPhoneNumbers(t.OwnerID);
-                if (nums.Count == 0)                
-                {
-                    sms = "Terrasoft: not found " + Transliter.Transliterate(DataConnector.Instance.GetContactName(t.OwnerID));
-                    string AdminPhone = System.Configuration.ConfigurationManager.AppSettings["admin_phone"];
-                    GSMGate.SendSms(AdminPhone, sms);                 
-                }
-                foreach (string num in nums)
-                {             
-                    GSMGate.SendSms(num, sms);
-                }
-                
-            }            
+            TaskSmsSender sender = new TaskSmsSender();
+            sender.SendSms(args);
         }
     }
 }
